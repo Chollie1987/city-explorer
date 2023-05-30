cb2eff2e31492754367fb02ee13a3898776543d9
Christina Hollie <christinahollie7@gmail.com>
2023-05-30 16:49:56 -0700

    fixed movies

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ class App extends React.Component {
         <Header />
         <Main />
         <Footer />
-        <Movies />
       </Container>
       </>
     )
--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,7 @@ import { Button } from "react-bootstrap";
 import Map from "./Map";
 import axios from "axios";
 import Weather from "./Weather";
+import Movies from "./Movies";
 
 
 class Main extends React.Component {
@@ -40,25 +41,27 @@ class Main extends React.Component {
     handleExplore = async (e) => {
         e.preventDefault();
         let url = `http://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.city}&format=json`
-        console.log(url);
         try {
         const response = await axios.get(url)
         const location = response.data[0]
         const mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${location.lat},${location.lon}&zoom=12`
+        let movies = await this.getMovieData()
             this.setState({
+                movieData: movies,
                 displayInfo: true, hasError: false,
-        cityName: response.data[0].display_name, mapUrl, lat:location.lat, lon:location.lon})
-        this.getWeather(location.lat, location.lon)
+                cityName: response.data[0].display_name, mapUrl, lat:location.lat, lon:location.lon})
+                this.getWeather(location.lat, location.lon)
         } catch (e){
             this.setState({hasError:true})
         }
+        
     }
 
     getMovieData = async() => {
-        let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIEDB_SERVER_KEY}&query=${this.state.movieData}`
+        let url = `${process.env.REACT_APP_SERVER_URL}/movies?city=${this.state.city}`
         try {
             const response = await axios.get(url)
-            this.setState({movieData:response.data})
+            return response.data
         } catch (e) {
             this.setState({
                 alertError: true
@@ -67,6 +70,7 @@ class Main extends React.Component {
     }
 
     render() {
+        
         return (
         <>
        
@@ -87,6 +91,7 @@ class Main extends React.Component {
             </>
         }
         {this.state.weatherData.length > 0 && <Weather weatherData={this.state.weatherData} />}
+        {this.state.movieData.length > 0 && <Movies movieData= {this.state.movieData}/>}
         {this.state.hasError && <h6>could not complete request</h6>}
         </>
         )
